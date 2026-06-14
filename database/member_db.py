47a7ae10426bf34f22a6adf137628fd427fc4e1b
ariel <arielkanner1@gmail.com>
2026-06-14 11:29:24 +0300

    with members class

--- database/member_db.py
+++ database/member_db.py
@@ -22,9 +22,78 @@ class MemberDB():
         cursor.close()
         conn.close()
 
+    def get_all_members(self):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute('SELECT * FROM members')
+        rows = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return rows
     
+    def get_member_by_id(self, id):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute('SELECT * FROM members WHERE id = %s', (id,))
+        row = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return row
+    
+    def deactivate_member(self, id):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute('UPDATE members SET is_active = 0 WHERE id = %s', (id,))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def activate_member(self, id):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute('UPDATE members SET is_active = 1 WHERE id = %s', (id,))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+    
+    def increment_borrows(self, id):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute('UPDATE members SET total_borrows = total_borrows + 1  WHERE id = %s', (id,))
+        conn.commit()
+
+        cursor.close()
+        conn.close()
+
+    def count_active_members(self):
+        conn = get_connection()
+        cursor = conn.cursor(dictionary=True)
+
+        cursor.execute('SELECT COUNT(*) FROM members WHERE is_active = 1')
+        rows = cursor.fetchall()
+
+        cursor.close()
+        conn.close()
+
+        return rows[0]['COUNT(*)']
+    
+    def get_top_member(self):
+        pass
 
 mem = MemberDB()
-data = {'name':'avi', 'email': 'avi@gmail'}
-data = Member(**data)
-mem.create_member(data)
+# data = {'name':'ari', 'email': 'ari@gmail'}
+# data = Member(**data)
+# mem.create_member(data)
+
+mem.activate_member(2)
